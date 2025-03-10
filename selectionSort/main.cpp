3d2f4337f688f9f920bf0445ed8b0ab047603ec6
oasis_pandey <oasispandey2000@gmail.com>
2025-03-09 22:55:29 -0500

    added selection sort

---
+++ selectionSort/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& arr){
+    int n = arr.size();
+
+    for(int i=0; i<=n-2;i++){
+        int minIdx = i;
+        for(int j=i+1; j<=n-1; j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+    
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    vector<int> arr = {3,2,7,4,10,1};
+    selectionSort(arr);
+
+    return 0;
+}
